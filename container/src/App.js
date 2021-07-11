77eb753884e997fc048a381d162f7fccef15922b
a.khademian <khademianamin@gmail.com>
2021-07-11 10:57:43 +0430

    Initial commit

---
+++ container/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import MarketingApp from "../components/marketingApp"
+
+export default () => {
+  return (
+    <div>
+      <h1>My Container</h1>
+      <MarketingApp />
+    </div>
+  )
+}
