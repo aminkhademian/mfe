d5b348cd2ac53be8d216bc52859f69f414933b3d
a.khademian <khademianamin@gmail.com>
2021-07-13 15:36:13 +0430

    publicPath improvement

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -8,14 +8,15 @@ const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = {
   mode: "production",
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
   plugins:[
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        auth: `auth@${domain}/auth/remoteEntry.js`,
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
       },
       shared: packageJson.dependencies
     })
